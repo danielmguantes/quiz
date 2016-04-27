f63f6a070c9467221765680ac9f08f89b9aff67f
daniel guantes <danielmguantes@gmail.com>
2016-04-27 23:18:42 +0200

    express-partials

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
+
+//router.get('/autor', function(req, res, next) {
+//  res.render('autor');
+//});
 
 module.exports = router;
